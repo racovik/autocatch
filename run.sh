718afa1989033739c17b4df5b87f08952ca3f56d
Kameil <racovikpvpbr@gmail.com>
2026-02-17 14:50:30 -0300

    fix discord.py-self 2.0.0 bug

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ echo "--- Atualizando... ---"
 git pull --rebase --autostash
 
 
+
 if [ -f "venv/bin/activate" ]; then
 
   source venv/bin/activate
@@ -29,6 +30,9 @@ else
   source venv/bin/activate
   pip install -r requirements.txt
 fi
+
+# fix discord.py-self 2.0.0 bug
+sed -i "s/data.get('pending_payments', \[\])/data.get('pending_payments') or \[\]/g" /data/data/com.termux/files/home/autocatch/venv/lib/python3.12/site-packages/discord/state.py
 
 echo "--- Iniciando o Bot ---"
 python discord_bot.py
