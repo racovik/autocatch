2f9efc700a497780573ffdec65d23f5911913974
Kameil <racovikpvpbr@gmail.com>
2026-03-23 15:28:05 -0300

    bot reestruturado + pokemon_bot support

--- discord_bot.py
+++ discord_bot.py
@@ -5,6 +5,9 @@ from io import BytesIO
 import os
 import re
 import json
+import argparse
+from concurrent.futures import ThreadPoolExecutor
+from typing import Callable
 
 import discord
 import httpx
@@ -14,7 +17,6 @@ from discord.ext import commands
 from PIL import Image
 from torchvision import models, transforms
 
-from config import guild_id
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s"
@@ -24,57 +26,150 @@ logging.basicConfig(
 description = "Autocatch"
 
 
-bot = commands.Bot(command_prefix="?", description=description, self_bot=True)
-# ----- CONFIG -----
-EMBEDDINGS_PATH = "models/classifier.pt" # model
-assert os.path.exists(EMBEDDINGS_PATH), f"{EMBEDDINGS_PATH} not found. please download model"
+class Autocatcher(commands.Bot):
+    def __init__(
+        self,
+        emb_model_path,
+        target_guild_id,
+        is_sending_rank,
+        pokemon_bot_support,
+        is_realm_disabled,
+    ) -> None:
+        super().__init__(command_prefix="?", description=description, self_bot=True)
 
-DEVICE = (
-    "cuda" if torch.cuda.is_available() else "cpu"
-)  # ou "cuda"/"rocm" se estiver usando GPU
-logging.info(f"Torch device: {DEVICE}")
-if DEVICE == "cuda":
-    logging.info(f"Cuda Device: {torch.cuda.current_device()}")
+        # configs do model
+        self.EMBEDDINGS_PATH = emb_model_path
+        self.model_device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model, self.base_embeddings = load_model(
+            self.model_device, self.EMBEDDINGS_PATH
+        )
+        self.transform = transforms.Compose(
+            [
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
 
-IMAGE_SIZE = 224
+        assert os.path.exists(self.EMBEDDINGS_PATH), (
+            f"{self.EMBEDDINGS_PATH} not found in files."
+        )
 
-# ISSO TEM Q SER IGUAL AO DO TREINAMENTO, ENT N MUDAR
-transform = transforms.Compose(
-    [
-        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
-        transforms.ToTensor(),
-        # Normalização padrão do ImageNet (Média e Desvio Padrão)
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
-# ----- MODELO BASE -----
-model = models.mobilenet_v2(
-    weights=models.MobileNet_V2_Weights.DEFAULT
-)  # MobileNEt Modelo feito para uso no Mobile
-model.classifier = torch.nn.Identity()
-model.eval()  # model.eval() feito para parar o treinamento do modelo, pelo q eu entendi
-model.to(DEVICE)
+        self.is_sending_rank = is_sending_rank
+        self.executor = ThreadPoolExecutor(max_workers=2)
+        self.target_guild_id = target_guild_id
+        self.http_client = httpx.Client()
+        self.queue = asyncio.Queue()
+        self.pokemon_bot_support = pokemon_bot_support
+        self.is_realm_disabled = is_realm_disabled
 
-# ----- LOAD DOS EMBEDDINGS -----
-base_embeddings = torch.load(EMBEDDINGS_PATH, map_location=DEVICE, weights_only=False)
-logging.info(f"Embeddings model loaded (Path: {EMBEDDINGS_PATH})")
+    async def setup_hook(self) -> None:
+        self.loop.create_task(self.process_queue())
+
+    async def process_queue(self):
+        while True:
+            item = await self.queue.get()
+            message: discord.Message
+            image_url: str
+            message, image_url, command = item
+
+            try:
+                async with message.channel.typing():
+                    emb = await self.loop.run_in_executor(
+                        self.executor,
+                        get_embedding_from_url,
+                        image_url,
+                        self.http_client,
+                        self.model,
+                        self.transform,
+                        self.model_device,
+                    )
+
+                    nome, score, rank = await self.loop.run_in_executor(
+                        self.executor, identify_pokemon, emb, self.base_embeddings
+                    )
+
+                    pokemon = format_pokemon_name(nome)
+                    await message.channel.send(f"{command} {pokemon}")
+
+            except Exception as e:
+                logging.error(f"Erro no worker: {e}")
+            finally:
+                self.queue.task_done()
+
+    async def on_ready(self):
+        logging.info(f"Logged in as {bot.user} (ID: {bot.user.id})")  # type: ignore
+        print("------")
+
+    async def on_message(self, message: discord.Message):
+        if isinstance(message.channel, discord.TextChannel):
+            if message.channel.guild.id == self.target_guild_id:  # type: ignore
+                if message.embeds:
+                    embed = message.embeds[0]
+                    title_clean = (embed.title or "").lower()
+                    if "wild" in title_clean and "pokémon" in title_clean:
+                        image_url = embed.image.url
+                        if embed.image is None:
+                            return
+                        if (
+                            message.author.id == 665301904791699476
+                            and not self.is_realm_disabled
+                        ):
+                            command = "<@665301904791699476> c"
+                            await self.queue.put((message, image_url, command))
+                        if (
+                            message.author.id == 669228505128501258
+                            and self.pokemon_bot_support
+                        ):
+                            command = ".catch"
+                            await self.queue.put((message, image_url, command))
+
+    async def close(self):
+        await super().close()
+        self.http_client.close()
+        self.executor.shutdown()
+
+
+def load_model(
+    device,
+    emb_path,
+):
+    model = models.mobilenet_v2(
+        weights=models.MobileNet_V2_Weights.DEFAULT
+    )  # MobileNEt Modelo feito para uso no Mobile
+    model.classifier = torch.nn.Identity()  # type: ignore
+    model.eval()  # model.eval() feito para parar o treinamento do modelo, pelo q eu entendi
+    model.to(device)
+
+    # ----- LOAD DOS EMBEDDINGS -----
+    base_embeddings = torch.load(emb_path, map_location=device, weights_only=False)
+    logging.info(f"Embeddings model loaded (Path: {emb_path}) (Device: {device}")
+    return model, base_embeddings
 
 
 # white backgroud pois no preto a IA poderia confundir as bordas dos pokemons com o fundo preto.
-def process_with_white_background(image: bytes):
+def process_with_white_background(image: bytes) -> Image.Image:
     img_rgba = Image.open(BytesIO(image)).convert("RGBA")
     fundo_branco = Image.new("RGBA", img_rgba.size, (255, 255, 255, 255))
     img_final = Image.alpha_composite(fundo_branco, img_rgba)
     return img_final.convert("RGB")
 
 
-def get_embedding_from_url(url):
+def get_embedding_from_url(
+    url: str,
+    http_client: httpx.Client,
+    model: torch.nn.Module,
+    transform: Callable[[Image.Image], torch.Tensor],
+    torch_device: str,
+):
     logging.debug(f"Getting image embedding from {url}")
-    response = httpx.get(url, timeout=10)
+    response = http_client.get(url, timeout=10)
     response.raise_for_status()
     img = process_with_white_background(response.content)
     # img = Image.open(BytesIO(response.content)).convert("RGB")
-    img = transform(img).unsqueeze(0).to(DEVICE)
+    img = transform(img).unsqueeze(0).to(torch_device)
 
     with torch.no_grad():
         emb = model(img)
@@ -82,12 +177,6 @@ def get_embedding_from_url(url):
     emb = emb.squeeze()
     emb = emb / torch.norm(emb)  # normalização
     return emb
-
-
-@bot.event
-async def on_ready():
-    logging.info(f"Logged in as {bot.user} (ID: {bot.user.id})")
-    print("------")
 
 
 # identify pokemon padrãozinho
@@ -104,7 +193,7 @@ async def on_ready():
 #     return melhor_pokemon, melhor_score
 
 
-def identify_pokemon(embedding):
+def identify_pokemon(embedding, base_embeddings):
     resultados = []
 
     # 1. Calcula o score para TODOS da base
@@ -134,52 +223,41 @@ def save_message_log(data: dict):
     with open(f"logs/{message_id}.json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
+
 def format_pokemon_name(pk) -> str:
     """remover informacoes da pasta. ex 'minior [core]' -> 'minior'"""
     return re.sub(r"(\[.*?\])", "", pk).strip()
 
-ranking = False
+
+def get_args():
+    argument_parser = argparse.ArgumentParser(description="just a autocatch")
+    argument_parser.add_argument(
+        "-m",
+        "--model-path",
+        type=str,
+        default="models/classifier.pt",
+        dest="emb_model_path",
+    )
+    argument_parser.add_argument(
+        "--ap", "--active-pokemon", action="store_true", dest="is_pokemon_bot_supported"
+    )
+    argument_parser.add_argument(
+        "--dr--deactive-pokerealm", action="store_true", dest="is_realm_disabled"
+    )
+    argument_parser.add_argument("-r", "--ranking", action="store_true")
+    return argument_parser.parse_args()
 
 
-@bot.event
-async def on_message(message: discord.Message):
-    if message.author.id == 665301904791699476:
-        if message.channel.guild.id == guild_id:
-            if message.embeds:
-                embed = message.embeds[0]
-                if embed.title and "A wild pokémon has аppeаred!" in embed.title:
-                    async with message.channel.typing():
-                        image_url = embed.image.url
-                        melhor_pokemon = None
-                        melhor_score = -1
-                        sleep_time = random.uniform(3, 5)
-                        await asyncio.sleep(sleep_time)
-                        embed_image_emb = await asyncio.to_thread(
-                            get_embedding_from_url, image_url
-                        )
-                        (
-                            melhor_pokemon,
-                            melhor_score,
-                            ranking_str,
-                        ) = await asyncio.to_thread(identify_pokemon, embed_image_emb)
-                        pokemon = format_pokemon_name(melhor_pokemon)
-                        await message.channel.send(
-                            f"<@665301904791699476> c {pokemon}"
-                        )
+if __name__ == "__main__":
+    from config import token
+    from config import guild_id
 
-                        logging.info(f"Identified pokemon: {melhor_pokemon}")
-                        data = {
-                            "message_id": message.id,
-                            "best_score": melhor_score,
-                            "best_match": melhor_pokemon,
-                            "ranking_str": ranking_str
-                        }
-                        save_message_log(data)
-                        if ranking:
-                            await asyncio.sleep(1)
-                            await message.reply(ranking_str)
-
-
-from config import token
-
-bot.run(token)
+    args = get_args()
+    bot = Autocatcher(
+        emb_model_path=args.emb_model_path,
+        is_sending_rank=args.ranking,
+        target_guild_id=guild_id,
+        pokemon_bot_support=args.is_pokemon_bot_supported,
+        is_realm_disabled=args.is_realm_disabled,
+    )
+    bot.run(token)
